b711c5a40cf3b5e984dd5b206d916810de914395
Brenton Partridge <bapartridge@gmail.com>
2013-12-08 12:43:38 -0800

    Argtable for Yaafe

--- Library/Formula/argtable.rb
+++ Library/Formula/argtable.rb
@@ -1,14 +1,16 @@
 require 'formula'
 
 class Argtable < Formula
-  homepage 'http://argtable.sourceforge.net'
-  url 'http://downloads.sourceforge.net/project/argtable/argtable/argtable-2.13/argtable2-13.tar.gz'
+  url 'http://sourceforge.net/projects/argtable/files/argtable/argtable-2.13/argtable2-13.tar.gz'
+  homepage 'http://argtable.sourceforge.net/'
+  md5 '156773989d0d6406cea36526d3926668'
   version '2.13'
-  sha1 'bce828c64c35e16f4c3f8e1f355e4a2a97fe3289'
 
   def install
-    system "./configure", "--disable-debug", "--disable-dependency-tracking",
+    system "./configure", "--disable-debug",
+                          "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
+    system "make"
     system "make install"
   end
 end
